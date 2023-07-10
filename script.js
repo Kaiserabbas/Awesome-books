f518100f6a44b35c6528e3ca1f93f74d8ac26cf9
Titus Lagat <lagattyto@gmail.com>
2023-07-10 10:58:16 +0300

    Add code to show inputs added by user

--- script.js
+++ script.js
@@ -33,3 +33,39 @@ function removeBook(index) {
   // Update the book list
   showBooks();
 }
+
+// Function to display the books in the list
+function showBooks() {
+  const bookList = document.getElementById('bookList');
+  bookList.innerHTML = "";
+  // Clear the existing list
+
+  // Create a new list item for each book
+  for (let i = 0; i < books.length; i++) {
+    const book = books[i];
+    const listItemTitle = document.createElement('p');
+    const listItemAuthor = document.createElement('p');
+
+    listItemTitle.innerHTML = book.title;
+    listItemAuthor.innerHTML = book.author;
+
+    // Create a button to remove the book
+    const removeButton = document.createElement("button");
+    removeButton.innerHTML = "Remove";
+    removeButton.addEventListener("click", function() {
+    removeBook(i);
+    });
+    const listItemHr  = document.createElement('hr');
+
+    // Append the books, author and remove button to the book list
+    bookList.appendChild(listItemTitle);
+    bookList.appendChild(listItemAuthor);
+    bookList.appendChild(removeButton);
+    bookList.appendChild(listItemHr);
+  }
+}
+
+// Add event listener to the form submission
+const bookForm = document.getElementById("form");
+bookForm.addEventListener('submit', addBooks);
+bookForm.addEventListener('submit', showBooks);
