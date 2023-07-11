5931f396d36eeb0e31c8746c4c81d34e91a972b2
Qaisar <kayser.abbas@gmail.com>
2023-07-11 12:59:29 +0500

    Add code of class in script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 /* eslint-disable no-plusplus */
 // Retrieve books from local storage if available
-const books = JSON.parse(localStorage.getItem('books')) || [];
+let books = JSON.parse(localStorage.getItem('books')) || [];
 
 // Function to save books to local storage
 function saveBooks() {
   localStorage.setItem('books', JSON.stringify(books));
+}
+// defining a class for form inputs 
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 // createing a function to add books by user through form inputs
 function addBooks(event) {
@@ -12,24 +19,25 @@ function addBooks(event) {
   event.preventDefault();
 
   // getting the input values against variables
-  const bookTitleInput = document.getElementById('book');
-  const authorInput = document.getElementById('author');
+  let titleInput = document.getElementById('title');
+  let authorInput = document.getElementById('author');
+  let title = titleInput.value;
+  let author = authorInput.value;
 
   // creating a books object
-  const book = {
-    title: bookTitleInput.value,
-    author: authorInput.value,
-  };
+  let book = new Book(title, author);
+  // {
+  // };
   // adding book to the books array
   books.push(book);
-
+  
   // clearing the inputs values after user clicks add button.
-  bookTitleInput.value = '';
+  titleInput.value = '';
   authorInput.value = '';
-
+  
   // updating the books list
   showBooks();
-
+  
   // Save the books to local storage
   saveBooks();
 }
@@ -45,38 +53,43 @@ function removeBook(index) {
 }
 
 // Function to display the books in the list
+
 function showBooks() {
-  const bookList = document.getElementById('added-books');
-  bookList.innerHTML = '';
+  let bookCount = 0;
+  let bookList = document.getElementById('added-books');
   // Clear the existing list
+  bookList.innerHTML = '';
 
   // Create a new list item for each book
   for (let i = 0; i < books.length; i++) {
-    const book = books[i];
-    const listItemTitle = document.createElement('p');
-    const listItemAuthor = document.createElement('p');
-
-    listItemTitle.innerHTML = book.title;
-    listItemAuthor.innerHTML = book.author;
+    let book = books[i];
+    let booksDiv = document.createElement('div');
+    booksDiv.setAttribute('class', 'books-div');
+    bookCount++;
+    if (bookCount % 2 === 0) {
+      booksDiv.style.backgroundColor = "white";
+    }
+    let listItemTitle = document.createElement('p');
+    listItemTitle.innerHTML = `"${book.title}" by ${book.author}`;
+    booksDiv.appendChild(listItemTitle);    
 
     // Create a button to remove the book
-    const removeButton = document.createElement('button');
+    let removeButton = document.createElement('button');
     removeButton.innerHTML = 'Remove';
     removeButton.addEventListener('click', function () {
       removeBook(i);
     });
-    const listItemHr = document.createElement('hr');
+    booksDiv.appendChild(removeButton);
+    // const listItemHr = document.createElement('hr');
 
     // Append the books, author and remove button to the book list
-    bookList.appendChild(listItemTitle);
-    bookList.appendChild(listItemAuthor);
-    bookList.appendChild(removeButton);
-    bookList.appendChild(listItemHr);
+    bookList.appendChild(booksDiv);
+    // bookList.appendChild(listItemHr);
   }
 }
 
 // Add event listener to the form submission
-const bookForm = document.getElementById('form');
+let bookForm = document.getElementById('form');
 bookForm.addEventListener('submit', addBooks);
 bookForm.addEventListener('submit', showBooks);
 showBooks();
